e487b40ce09604597a5caa65d4af79ac9832aa9f
orestispanago <orestispanago@gmail.com>
2022-07-20 22:40:59 +0300

    Added plotting functions, working on data selection

--- datareader.py
+++ datareader.py
@@ -1,12 +1,53 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 def read(fname):
     start = pd.to_datetime(fname.split(".")[0], format="%y%m%d_%H%M%S")
     df = pd.read_csv(fname)
     df['Time'] = start + pd.to_timedelta(df['Time'], unit='s')
     df.set_index('Time', inplace=True)
-    df =  df.resample('1s').mean()
+    df =  df.resample('1min').mean()
+    df = df.tz_localize('Europe/Berlin').tz_convert('UTC')
     return df
 
+
+def plot_col(col, ylabel=''):
+    col.plot(style='.')
+    plt.ylabel(ylabel)
+    plt.xlabel("Time (UTC)")
+    plt.grid(which='both')
+    plt.show()
+
+def plot_multiple(df, col_list, ylabel=''):
+    for col in col_list:
+        df[col].plot(style='.')
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.xlabel("Time (UTC)")
+    plt.grid(which='both')
+    plt.show()
+    
+    
 df = read("220719_122000.csv")
+
+df = df.loc['2022-07-19 10:40:00+0000' : '2022-07-19 14:00:00+0000']
+
+df['Tout_Tin'] = df['T_out'] - df['T_in']
+df['q_dot'] = 4.2 * df['m_dot'] * df['Tout_Tin']
+df['eff'] = df['q_dot']/(df['I_dir'] * 1.5) # TODO check collector dimensions
+
+df['Tin_Tamb'] = df['T_in'] - df['T_amb']
+
+plot_col(df['m_dot'], ylabel="$\dot m \ (g \cdot s^{-1})$")
+plot_col(df['I_dir'], ylabel='$DNI \ (W \cdot m^{-2})$')
+# plot_col(df['I_dif'], ylabel='$DHI \ (W \cdot m^{-2})$')
+# plot_multiple(df, ['I_dir', 'I_dif'], ylabel='$W \cdot m^{-2}$')
+plot_multiple(df, ['T_amb', 'T_in', 'T_out'], ylabel='$ \degree C$')
+
+plot_col(df['Tout_Tin'],  ylabel='$T_{out} - T_{in} \ (\degree C)$')
+# plot_col(df['eff'], ylabel="Efficiency")
+plot_col(df['q_dot'], ylabel='$\dot Q \ (W)$')
+
+df['T_in'].plot()
+df[df.diff()['T_in'] < 0.1]['T_in'].plot(style='.')
+plt.show()
